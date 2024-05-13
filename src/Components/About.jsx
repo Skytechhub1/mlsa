578012d177ed1ec009bef18617fb822122795bad
Skytechhub1 <sukeshyadav9290@outlook.com>
2024-05-13 23:59:49 +0530

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/paint.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Beautiful paint colours";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "My name is Sky and I am a student of B.TEch.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Gaming profile",
+  "My experience",
+  "Web designer",
+  "Focus testing",
+  "Mobile users",
+  "Game designer",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "This is my protfolio for the my new site.";
 
 const About = () => {
   return (
